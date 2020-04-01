08f9c0647deb7aaf3f17abe68123acd550aaa8df
Ekaterina Zarudnaya <zar.catherine@gmail.com>
2020-04-01 19:40:21 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def turn_count(board)
   # board.count{|token| token == "X" || token == "O"}
 
@@ -13,4 +14,30 @@ end
 
 def current_player(board)
   turn_count(board) % 2 == 0 ? "X" : "O"
+=======
+#method takes in an argument of the board array and
+# returns the number of turns that have been played
+#require 'pry'
+
+def turn_count(board)
+ counter = 0
+ board.each do |token|
+   if token == "X" || token == "0"
+    counter += 1
+  #  binding.pry
+   end
+  end
+  counter
+end
+
+#takes in an argument of the game board and
+# uses the #turn_count method to determine if it is "X"'s turn or "O"'s.
+def current_player(board)
+  #turn_count(board) % 2 == 0? "X" : "0"
+ if turn_count(board).even?
+  return "X"
+ else
+  return "0"
+ end
+>>>>>>> 2aaf7a2b5f6c52cb3c54f336ac5999ab53081088
 end
